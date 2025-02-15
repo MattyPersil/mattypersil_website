7923b8d64356745f57208b69d492ea4f68c96ff6
MattyPersil <ananaze44@gmail.com>
2025-02-15 23:04:50 +0100

    Added $title variable to set the title each page

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ $header_content = '<li><a href="#top" id="top-link"><span class="icon solid fa-h
                    <li><a href="#portfolio" id="portfolio-link"><span class="icon solid fa-th">Portfolio</span></a></li>
                    <li><a href="#about" id="about-link"><span class="icon solid fa-user">About Me</span></a></li>
                    <li><a href="#contact" id="contact-link"><span class="icon solid fa-envelope">Contact</span></a></li>';
+$title = "MattyPersil Homepage";
 include "assets/php/header.php";
 ?>
 
